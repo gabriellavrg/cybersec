f9ef0593a96ecc5d2c681b66362789264d02df0b
Gabriella <89670224+gabriellavrg@users.noreply.github.com>
2022-05-14 23:34:11 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,11 +1,11 @@
 import sys
 import socket
 
-ipAddress = sys.argv[1]
+ip = sys.argv[1]
 
-for ports in range(1,65535):
+for p in range(1,65535):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    if s.connect_ex((ipAddress, ports)) == 0:
-        print(f"Porta {ports} Aberta!")
+    if s.connect_ex((ip, p)) == 0:
+        print(f"Porta {p} Aberta!")
         s.close()
