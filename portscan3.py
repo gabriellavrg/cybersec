232e55f5af4e4fee7955864345972c27dba03914
Gabriella <89670224+gabriellavrg@users.noreply.github.com>
2022-06-04 23:07:02 -0300

    Scan for the most common ports

---
+++ portscan3.py
@@ -0,0 +1,13 @@
+import socket
+
+
+ports = [80, 443, 20, 21, 22, 25, 53, 23, 69, 139, 137, 445, 8080, 8443]
+
+for p in ports:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.settimeout(0.1)
+	code = client.connect_ex(("bancocn.com", p))
+	if code == 0:
+		print(p, "OPEN")
+	else:
+		print(p, "CLOSED OR FILTERED")
