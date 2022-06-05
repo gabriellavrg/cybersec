c76031400f4e4918f1f00a414e69e5f21c05f81a
Gabriella <89670224+gabriellavrg@users.noreply.github.com>
2022-06-04 23:29:22 -0300

    Scan the most common/vulnerable ports

--- portscan2.py
+++ portscan2.py
@@ -2,11 +2,12 @@ import socket
 
 
 ports = [80, 443, 20, 21, 22, 25, 53, 23, 69, 139, 137, 445, 8080, 8443] 
+target = "example.com"
 
 for p in ports:
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.settimeout(0.1)
-	code = client.connect_ex(("example.com", p)) #site or IP
+	code = client.connect_ex((target, p)) #site or IP
 	if code == 0:
 		print(p, "OPEN")
 	else:
