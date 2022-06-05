78860593003957692a971a485d02a752cd91f87f
Gabriella <89670224+gabriellavrg@users.noreply.github.com>
2022-06-04 23:09:15 -0300

    Scan all open ports

--- portscan.py
+++ portscan.py
@@ -7,5 +7,5 @@ for p in range(1,65535):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     if s.connect_ex((ip, p)) == 0:
-        print(f"Porta {p} Aberta!")
+        print(f"Open {p} Port!")
         s.close()
