ab5761b09d433a9e1bc781b11670eb9e4aecd696
Gabriella <89670224+gabriellavrg@users.noreply.github.com>
2022-06-04 23:09:36 -0300

    Delete portscan2.py

--- portscan2.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("exemplo_de_alvo.com.br", númerodaporta))
-client.send(b"um_texto_qualquer")
-response = client.recv(1024) #quantidade de bytes a serem recebidos
-print(response.decode()) #saída como no netcat
